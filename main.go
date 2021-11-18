8248d04cb4aee9b4f397a6cf3645951b7b6495e5
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-11-18 18:19:49 +0000

    Kata 2

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	hiddenWord "elenamsd/kata-go/hiddenWord"
+	"elenamsd/kata-go/stringCalculator"
 	"fmt"
 )
 
 func main() {
 
 	fmt.Println(hiddenWord.HiddenWord(1))
+	a := stringCalculator.Add("1,2")
+	fmt.Println(a)
 }
